d4d8a9e9db66969ed62eb47da563038812bfb78b
Hugo Reyes <31859922+winstonthewolf@users.noreply.github.com>
2023-07-04 07:57:12 -0700

    Started Code on this app

---
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.example.artspace
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ElevationOverlay
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.artspace.ui.theme.ArtSpaceTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ArtSpaceTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background,
+
+                ) {
+                    Artspace()
+                }
+            }
+        }
+    }
+}
+
+
+
+@Composable
+fun Artspace(){
+
+    Column(
+
+        modifier = Modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Image(painter = painterResource(R.drawable.pb121986), contentDescription = "image",
+             modifier = Modifier
+                 .border(12.dp, Color.Blue, RectangleShape),
+            contentScale = ContentScale.FillWidth
+            )
+
+        Text(
+            text = "Los Angeles Street",
+            modifier = Modifier
+                .padding(bottom = 16.dp)
+        )
+        
+        Text(text = "Hugh Reyes (2022)")
+
+
+
+
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ArtSpaceTheme {
+        Artspace()
+    }
+}
